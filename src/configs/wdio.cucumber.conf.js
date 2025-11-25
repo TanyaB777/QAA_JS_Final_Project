88573c9759fcfd3199ae9aae88d08a29d7ac8012
Tanya <tanyawbw7@gmail.com>
2025-11-25 18:06:46 +0200

    Config files were changed

--- src/configs/wdio.cucumber.conf.js
+++ src/configs/wdio.cucumber.conf.js
@@ -10,26 +10,5 @@ exports.config = {
         require: ['./src/step-definitions/*.steps.js',
              './src/features/support/hooks.js'],
         timeout: 60000,
-    },
-    beforeTest: function (test, context) {
-        console.log(`TEST STARTED: ${test.title} on ${browser.capabilities.browserName}`);
-    },
-    afterTest: async function(test, context, { error, result, duration, passed, retries }) {
-        if (error) {
-            console.log(`TEST FAILED: ${test.title} (${duration}ms) with error "${error.message}"`);
-
-            const filename = `${test.title.replace(/\s+/g, '_')}_${Date.now()}.png`;
-            const dirPath = './artifacts/screenshots/';
-        
-            if (!existsSync(dirPath)) {
-                mkdirSync(dirPath, {
-                    recursive: true,
-                });
-            }
-
-            await browser.saveScreenshot(dirPath + filename);
-        }
-        else
-            console.log(`TEST FINISHED: ${test.title} (${duration}ms)`);
     }
 };
